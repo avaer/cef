bddf8b09523f63fc0114d063b378b118e48035e5
Avaer Kazmer <a@modules.io>
2018-11-05 02:02:02 -0500

    Add CEF framework install name change hack to macOS build script

--- build/macos/build-macos.sh
+++ build/macos/build-macos.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# install_name_tool -id '@rpath/Chromium Embedded Framework.framework/Chromium Embedded Framework' node_modules/native-browser-deps/lib3/macos/Chromium\ Embedded\ Framework.framework/Chromium\ Embedded\ Framework
+
 cmake .
 make -j3
