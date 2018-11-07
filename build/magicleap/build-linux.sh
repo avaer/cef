75f48170e74b3bb65b856ea50924bff33fd2269a
Avaer Kazmer <a@modules.io>
2018-11-07 01:20:59 -0500

    Major magic leap build refactoring

--- build/magicleap/build-linux.sh
+++ build/magicleap/build-linux.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
-export PATH="/home/a/code/chromium_git/chromium/src/third_party/llvm-build/Release+Asserts/bin:$PATH"
-cmake -DCMAKE_TOOLCHAIN_FILE=./toolchains
-make -j2 VERBOSE=1
+# export PATH="/home/a/code/chromium_git/chromium/src/third_party/llvm-build/Release+Asserts/bin:$PATH"
+# cmake -DCMAKE_TOOLCHAIN_FILE=./toolchains
+# make -j2 VERBOSE=1
+
+set -e
+
+export MLSDK=${MLSDK:-/mnt/c/Users/avaer/MagicLeap/mlsdk/v0.16.0}
+
+# pushd ..
+# npm install native-video-deps
+# popd
+
+if [ ! -d magicleap-js ]; then
+  git clone https://github.com/webmixedreality/magicleap-js
+else
+  pushd magicleap-js
+  git pull --rebase
+  popd
+fi
+
+./magicleap-js/hack-toolchain.js
+
+cmake -D CMAKE_TOOLCHAIN_FILE=.\\toolchains -D LUMIN=1 .
+# make clean
+make -j4
